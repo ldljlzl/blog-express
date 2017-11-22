57035bb9e29b0e0d9079ba705c1e8bd51f263c4d
lzl <805298331@qq.com>
2017-11-22 22:42:41 +0800

    20171122

--- public/js/admin/users.js
+++ public/js/admin/users.js
@@ -1,5 +1,17 @@
 $(function(){
-    $("img.edit").click(function(elem){
-        $(this).parent().prev()[0].style.color='red'
+    $("td.textEdit").hide()
+    $("img.edit").click(function(){
+        $(this).parent().hide()
+        $(this).parent().siblings('td.text').hide()
+        $(this).parent().siblings('td.textEdit').show()
+    })
+    $("a.dropMenu").click(function(){
+        $("div.isAdminText").text($(this).text())  
+    })
+    $("img.save").click(function(){
+
+        $(this).parent().hide()
+        $(this).parent().siblings('td.text').hide()
+        $(this).parent().siblings('td.textEdit').show()
     })
 })
