52279c6d8a65f7e291b270edda180b4741945b90
lzl <805298331@qq.com>
2017-11-23 21:27:01 +0800

    20171123

--- public/js/admin/users.js
+++ public/js/admin/users.js
@@ -1,17 +1,29 @@
 $(function(){
+    let oldAccount
     $("td.textEdit").hide()
     $("img.edit").click(function(){
         $(this).parent().hide()
         $(this).parent().siblings('td.text').hide()
         $(this).parent().siblings('td.textEdit').show()
+        oldAccount=$(this).parent().siblings('td.account').children('input').val()
     })
     $("a.dropMenu").click(function(){
         $("div.isAdminText").text($(this).text())  
     })
     $("img.save").click(function(){
-
-        $(this).parent().hide()
-        $(this).parent().siblings('td.text').hide()
-        $(this).parent().siblings('td.textEdit').show()
+        let _this=this
+        
+        let account=$(this).parent().siblings('td.account').children('input').val()
+        let password=$(this).parent().siblings('td.password').children('input').val()
+        let isAdmin=$(this).parent().siblings('td.isAdmin ').children('div.isAdminText').text()
+        $.post('/admin/users/save',{
+            oldAccount:oldAccount,
+            account:account,
+            password:password,
+            isAdmin:isAdmin
+        },function(data){
+            alert(data)
+            location.reload()
+        })
     })
 })
