ce6726497c25dedd6d460f96db707a5f49aed370
lzl <805298331@qq.com>
2017-11-30 23:10:52 +0800

    20171130/2

--- router/index.js
+++ router/index.js
@@ -10,42 +10,41 @@ let router=express.Router()
 
 router.get('/', function(req, res,next) {
     _res=res
-    let myCategory
-    let article
-    Category.find(function(err,res){
-        if(err){
-            console.log('分类查找数据库失败')
-            _res.send('分类查找数据库失败')
-            return
-        }else{
-            myCategory=res
-        }
-    })
-    Article.find(function(err,res){
-        if(err){
-            console.log('文章查找数据库失败')
-            _res.send('文章查找数据库失败')
-            return
-        }else{
-            article=res
-        }
-    })
-    if(article&&myCategory){
-        if(req.cookies.userinfo){
-            res.render('index1',{
-                username:req.cookies.userinfo.username,
-                article:article,
-                category:myCategory
-            })
-        }
-        else{
-            res.render('index1',{
-                article:article,
-                category:myCategory  
-            })
-        }
-        next()
-    }
+    res.render('index1')
+
+    // let myCategory
+    // let article
+    // Category.find(function(err,res){
+    //     if(err){
+    //         console.log('分类查找数据库失败')
+    //         _res.send('分类查找数据库失败')
+    //         return
+    //     }else{
+    //         myCategory=res
+    //         Article.find(function(err,res){
+    //             if(err){
+    //                 console.log('文章查找数据库失败')
+    //                 _res.send('文章查找数据库失败')
+    //                 return
+    //             }else{
+    //                 article=res
+    //                 if(req.cookies.userinfo){
+    //                     _res.render('index1',{
+    //                         username:req.cookies.userinfo.username,
+    //                         article:article,
+    //                         category:myCategory
+    //                     })
+    //                 }
+    //                 else{
+    //                     _res.render('index1',{
+    //                         article:article,
+    //                         category:myCategory  
+    //                     })
+    //                 }
+    //             }
+    //         })
+    //     }
+    // })
 })
 
 
